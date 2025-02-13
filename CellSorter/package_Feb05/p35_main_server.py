1b64bbdd5548af914f23a4c12945bb2b21995c42
Hemmiil <158091932+Hemmiil@users.noreply.github.com>
2025-02-13 17:15:00 +0900

    Add files via upload

---
+++ CellSorter/package_Feb05/p35_main_server.py
@@ -0,0 +1,12 @@
+from package_Feb05 import p03_Vis
+
+def main():
+    instance = p03_Vis.Vis()
+    instance.double_statistics_with_clustersize()
+    #instance.boxplots()
+    #instance.violineplots()
+    #instance.statistics_summary_mean()
+    #instance.statistics_summary_var()
+
+if __name__ == "__main__":
+    main()
